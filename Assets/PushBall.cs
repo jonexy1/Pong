76197619bad889057506a798882666e646c4721e
Craig <craigjones1198@gmail.com>
2021-11-21 00:48:19 +0000

    added a ball and made it bounce forever

---
+++ Assets/PushBall.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PushBall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private float thrust = 100f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+        rb.AddForce(transform.up * thrust);
+    }
+}
